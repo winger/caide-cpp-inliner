d9d259b4bfea048c5633ee339a9f9718197c1d04
slycelote <slycelot@yandex.ru>
2016-02-04 21:33:41 -0800

    Update travis config

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -ev
 
-#export CXX=g++-4.8
-#export CC=gcc-4.8
+export CXX=g++-4.8
+export CC=gcc-4.8
 
 git submodule init
-git submodule update src/clang
+git submodule update src/clang src/llvm
 
 mkdir build
 cd build
-cmake -DCAIDE_USE_SYSTEM_CLANG=ON ../src
+cmake -DCAIDE_USE_SYSTEM_CLANG=OFF ../src
 make -j2 cmd test-tool
 ctest
 
