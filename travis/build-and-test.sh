ead5e8f374608135ee3476facaa8f358367f838d
Slycelote <slycelote@users.noreply.github.com>
2016-02-04 19:45:16 -0800

    Update build-and-test.sh

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -ev
 
+g++ --version
+gcc --version
+cmake --version
+python --version
+
 mkdir build
 cd build
 cmake -DCAIDE_USE_SYSTEM_CLANG=ON ../src
 make -j2 cmd test-tool
 ctest
-
