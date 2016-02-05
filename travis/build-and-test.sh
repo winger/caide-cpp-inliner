cb4f1b11b185ff924158fd281f1c4b64caa864a4
slycelote <slycelot@yandex.ru>
2016-02-04 20:11:46 -0800

    Update travis config

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -6,8 +6,12 @@ gcc --version
 cmake --version
 python --version
 
+git submodule init
+git submodule update src/clang
+
 mkdir build
 cd build
 cmake -DCAIDE_USE_SYSTEM_CLANG=ON ../src
 make -j2 cmd test-tool
 ctest
+
